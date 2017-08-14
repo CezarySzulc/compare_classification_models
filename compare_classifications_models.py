4d17c6e2392fa40d7ccdadc3734dfaa5a4cf8e47
Cezary Szulc <cezary.szulc@gmail.com>
2017-08-14 19:02:00 +0200

    add k-neighbors clasificator

--- compare_classifications_models.py
+++ compare_classifications_models.py
@@ -5,25 +5,30 @@
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 class model():
     def __init__(self):
         self.data_set = load_iris()
-        print(self.data_set.target)
-        print(type(self.data_set))
         
     def prepare_data(self):
         """ prepare train and test set """
         
-        X_train, X_test, y_train, y_test = train_test_split(
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+                                            train_test_split(
                                                 self.data_set.data, 
                                                 self.data_set.target,
                                                 test_size=0.3,
                                                 random_state=43)
                                                 
     def learn_model(self):
-        """ """
-        pass
+        """ create and learn model """
+        
+        self.k_nei_clf = KNeighborsClassifier()
+        self.k_nei_clf.fit(self.X_train, self.y_train)
+        print(self.k_nei_clf.score(self.X_test, self.y_test))
     
 if __name__ == '__main__':
     ai = model()
+    ai.prepare_data()
+    ai.learn_model()
