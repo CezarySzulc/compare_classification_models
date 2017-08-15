30258caf284d552e32be2e44d5b0ed47aad4b98b
Cezary Szulc <cezary.szulc@gmail.com>
2017-08-15 15:01:32 +0200

    classification raport and confusion matrix

--- compare_classifications_models.py
+++ compare_classifications_models.py
@@ -11,6 +11,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import (
+                        classification_report, 
+                        confusion_matrix)
+
 
 class model():
     def __init__(self):
@@ -29,16 +33,28 @@ class model():
     def learn_model(self):
         """ create and learn model """
         
-        param_grid = {'n_neighbors': np.arange(3 ,5)}        
+        param_grid = {'n_neighbors': np.arange(1,10)}        
         self.k_nei_clf = GridSearchCV(KNeighborsClassifier(),
                                       param_grid=param_grid)
         self.k_nei_clf.fit(self.X_train, self.y_train)
-        print(self.k_nei_clf.score(self.X_test, self.y_test))
-        print(self.k_nei_clf.best_params_)
+        
         
         self.log_reg_clf = OneVsRestClassifier(LogisticRegression())
         self.log_reg_clf.fit(self.X_train, self.y_train)
-        print(self.log_reg_clf.score(self.X_test, self.y_test))
+        
+        
+        print(self.k_nei_clf.predict_proba(self.X_test))
+        predict = self.k_nei_clf.predict(self.X_test)
+        print(classification_report(self.y_test, predict))
+        print(confusion_matrix(self.y_test, predict))
+        print(self.k_nei_clf.best_params_)
+        
+        
+        pr_proba = np.array(self.log_reg_clf.predict_proba(self.X_test))
+        print(np.sum(pr_proba, axis=1))
+        predict = self.log_reg_clf.predict(self.X_test)
+        print(classification_report(self.y_test, predict))
+        print(confusion_matrix(self.y_test, predict))
     
 if __name__ == '__main__':
     ai = model()
