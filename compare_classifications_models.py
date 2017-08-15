fc73767c7a067c4b002ea61c47455c7eaa89fda4
Cezary Szulc <cezary.szulc@gmail.com>
2017-08-15 14:34:25 +0200

    searching best params for n-neighbors

--- compare_classifications_models.py
+++ compare_classifications_models.py
@@ -3,9 +3,14 @@
 @author: C
 """
 
+import numpy as np
+
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import GridSearchCV
 
 class model():
     def __init__(self):
@@ -24,9 +29,16 @@ class model():
     def learn_model(self):
         """ create and learn model """
         
-        self.k_nei_clf = KNeighborsClassifier()
+        param_grid = {'n_neighbors': np.arange(3 ,5)}        
+        self.k_nei_clf = GridSearchCV(KNeighborsClassifier(),
+                                      param_grid=param_grid)
         self.k_nei_clf.fit(self.X_train, self.y_train)
         print(self.k_nei_clf.score(self.X_test, self.y_test))
+        print(self.k_nei_clf.best_params_)
+        
+        self.log_reg_clf = OneVsRestClassifier(LogisticRegression())
+        self.log_reg_clf.fit(self.X_train, self.y_train)
+        print(self.log_reg_clf.score(self.X_test, self.y_test))
     
 if __name__ == '__main__':
     ai = model()
